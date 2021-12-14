e74ff17ca5a675056f2672a727d9fc3781faaa04
asimhafeezz <asimhaff.social@gmail.com>
2021-12-14 23:22:36 +0500

    login with metamask added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useLogin } from "./useLogin"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//useLogin hook
+	const { isLogin, account, error, loading, balance, login } = useLogin()
+
+	if (error) return <h3>{error}</h3>
+
+	if (loading) return <h3>Loading...</h3>
+
+	return (
+		<div className='App'>
+			<section>
+				{isLogin ? (
+					<section>
+						<h1>Account: {account}</h1>
+						<h1>Balance: {balance}</h1>
+					</section>
+				) : (
+					<button onClick={login}>Login with MetaMask</button>
+				)}
+			</section>
+		</div>
+	)
 }
 
-export default App;
+export default App
